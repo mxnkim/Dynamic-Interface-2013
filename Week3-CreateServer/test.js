d2fe75cb054cd33449720d9aed38369cb21b2f10
mxnkim <mxnkim@gmail.com>
2013-03-03 21:31:43 -0500

    Create Server

---
+++ Week3-CreateServer/test.js
@@ -0,0 +1,10 @@
+var http = require("http");
+var url = require("url");
+
+http.createServer(function(req,res){
+	var parsedUrl = url.parse(req.url);
+	console.log(parsedUrl);
+	console.log("Requested" + req.url);
+
+
+});.listen(8800,'127.0.0.1');
